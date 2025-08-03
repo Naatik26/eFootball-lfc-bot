2418ae14599b33c1d2e88307e7ce25800a1f452e
Naatik26 <84635227+Naatik26@users.noreply.github.com>
2025-08-04 00:50:48 +0300

    Update efb_bot.py

--- efb_bot.py
+++ efb_bot.py
@@ -73,8 +73,8 @@ async def menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     elif data == "list":
         if approved:
             names = [participants[uid]["name"] for uid in approved]
-            await update.callback_query.message.reply_text("✅ Ishtirokchilar:
-" + "\n".join(names)) """
+            await update.callback_query.message.reply_text("✅ Ishtirokchilar:"
+" + "\n".join(names))
         else:
             await update.callback_query.message.reply_text("Hali ishtirokchilar ro'yxati mavjud emas.")
     elif data == "submit_result":
